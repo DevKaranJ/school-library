242f5fe92a8eb2b80de27c798cdd6bf5789ce4b7
Karan Joshi <Karan190213@gmail.com>
2023-12-06 15:39:27 +0530

    rental.rb updated

--- rental.rb
+++ rental.rb
@@ -1,7 +1,6 @@
 # rental.rb
 class Rental
-  attr_accessor :date
-  attr_reader :person, :book
+  attr_accessor :date, :person, :book
 
   def initialize(date, person, book)
     @date = date
@@ -11,15 +10,5 @@ class Rental
     # Set the person and book for the rental
     person.add_rental(self)
     book.add_rental(self)
-  end
-
-  # Define a setter method for person
-  def person=(new_person)
-    @person = new_person
-  end
-
-  # Define a setter method for book
-  def book=(new_book)
-    @book = new_book
   end
 end
