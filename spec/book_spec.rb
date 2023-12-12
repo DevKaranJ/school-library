5e6121d66d017f3f93e9515220cbb4e40995de90
Karan Joshi <Karan190213@gmail.com>
2023-12-12 18:04:37 +0530

    book.spec passed

---
+++ spec/book_spec.rb
@@ -0,0 +1,19 @@
+# spec/book_spec.rb
+
+require_relative '../book'
+require_relative '../rental'
+
+describe Book do
+  let(:book_title) { 'The Great Gatsby' }
+  let(:author_name) { 'F. Scott Fitzgerald' }
+  let(:book) { Book.new(book_title, author_name) }
+
+  it 'has a title and an author' do
+    expect(book.title).to eq(book_title)
+    expect(book.author).to eq(author_name)
+  end
+
+  it 'starts with an empty list of rentals' do
+    expect(book.rentals).to be_empty
+  end
+end
